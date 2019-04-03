bfb1ce0d2d5c0e7c9c42c875cd60ae14c3bcec91
Gerardo Rodriguez <gerardo.rodriguez4306@gmail.com>
2019-04-02 20:32:37 -0500

    Added a to-do list and updated the client app

--- development_directory/chat_client.cpp
+++ development_directory/chat_client.cpp
@@ -157,21 +157,125 @@ int main(int argc, char* argv[])
     char formatted_message[chat_message::max_body_length + 1];
     // create a date and time string, copy into formatted_message, concatenate nick, then concatenate msg
     char line[chat_message::max_body_length + 1];
+    std::cout << "Welcome to the main lobby! Remember: no profanity, no Smurf attacks, and have fun!\n\nType #h for a list of commands\n\n" << std::endl;
     while (std::cin.getline(line, chat_message::max_body_length + 1))
     {
-      t_ = time(NULL);
-      strftime(time_buffer, 32, "%c", localtime(&t_));
-      sprintf(formatted_message, "%s %s: ", time_buffer, nick);
-      std::strcat(formatted_message, line);
-      chat_message msg;
-      msg.body_length(std::strlen(formatted_message));
-      std::memcpy(msg.body(), formatted_message, msg.body_length());
-      msg.encode_header();
-      c.write(msg);
-      //update formatted_message with date and time
-      t_ = time(NULL);
-      strftime(time_buffer, 32, "%c", localtime(&t_));
-      sprintf(formatted_message, "%s %s: ", time_buffer, nick);
+      if (line[0] == '#')
+      {
+        switch (line[1])
+        {
+          case 'h':
+            std::cout << "\n#h: help --   prints the list of respective commands and shows how to navigate the application\n\n\
+#e: encode -- obfuscates a message based on a Caesar cipher prior to being sent\n\n\
+#d: decode -- de-obfuscates a message based on a Caesar cipher at any given time\n\n\
+#b: block --  blocks a user from a chatroom; cannot be used in lobby\n\n\
+#l: lobby --  takes user back to lobby\n\n\
+#mc: make chatroom -- makes a chatroom\n\n\
+#dc: delete chatroom -- deletes a chatroom\n\n\
+#lo: logout -- logs a user out (basically ^C)\n\n\
+#f: file --   sends a file to all users in a chatroom\n\n\
+#sc: show chatrooms -- shows available chatrooms in lobby" <<std::endl;
+            break;
+          case 'e':
+            std::cout << "encoding..." << std::endl;
+            break;
+
+          case 'd':
+            if (line[2] == '\0')
+            {
+              std::cout << "decoding..." << std::endl;
+              break;
+            }
+            else if (line[2] == 'c' && line[3] == '\0')
+            {
+              std::cout << "deleting chatroom..." << std::endl;
+              break;
+            }
+            else
+            {
+              std::cout << line << " is not a command. Please try again." << std::endl;
+              break;
+            }
+          case 'b':
+            std::cout << "blocking user..." << std::endl;
+            break;
+
+          case 'l':
+            if (line[2] == '\0')
+            {
+              std::cout << "going to lobby..." << std::endl;
+              break;
+            }
+            else if (line[2] == 'o' && line[3] == '\0')
+            {
+              std::cout << "logging out...\n" << std::endl;
+              break;
+            }
+            else
+            {
+              std::cout << line << " is not a command. Please try again." << std::endl;
+              break;
+            }
+          case 'm':
+            std::cout << "Making chatroom..." << std::endl;
+            break;
+
+          case 'f':
+            std::cout << "File transfer in progress..." << std::endl;
+            break;
+
+          case 's':
+            std::cout << "Displaying available chatrooms..." << std::endl;
+            break;
+
+          default:
+            std::cout << line << " is not a command. Please try again." << std::endl;
+            break;
+        }
+        /*determine if it's a valid command and then execute the command
+
+          #h: help -- prints the list of respective commands and shows
+                      how to navigate the application
+
+          #e: encode -- obfuscates a message based on a Caesar cipher prior
+                      to being sent
+
+          #d: decode -- de-obfuscates a message based on a Caesar cipher
+                      at any given time
+
+          #b: block -- blocks a user from a chatroom; cannot be used in lobby
+
+          #l: lobby -- takes user back to lobby
+
+          #mc: make chatroom -- makes a chatroom
+
+          #dc: delete chatroom -- deletes a chatroom
+
+          #lo: logout -- logs a user out (basically ^C)
+
+          #f: file -- sends a file to all users in a chatroom
+
+          #sc: show chatrooms -- shows available chatrooms in lobby
+
+        */
+
+      }
+      else
+      {
+        t_ = time(NULL);
+        strftime(time_buffer, 32, "%c", localtime(&t_));
+        sprintf(formatted_message, "%s %s: ", time_buffer, nick);
+        std::strcat(formatted_message, line);
+        chat_message msg;
+        msg.body_length(std::strlen(formatted_message));
+        std::memcpy(msg.body(), formatted_message, msg.body_length());
+        msg.encode_header();
+        c.write(msg);
+        //update formatted_message with date and time
+        t_ = time(NULL);
+        strftime(time_buffer, 32, "%c", localtime(&t_));
+        sprintf(formatted_message, "%s %s: ", time_buffer, nick);
+      }
     }
 
     c.close();
---
+++ development_directory/required_tasks.md
@@ -0,0 +1,26 @@
+# To-Do List
+
+|                     Task                      | Taken | Completed |
+| :-------------------------------------------: | :----:| :-------: |
+| Code message obfuscation `#e`                 |   Y   |     Y     |
+| Code message de-obfuscation `#d`              |   Y   |     N     |
+| Code message de-obfuscation `#d`              |   Y   |     N     |
+| Code `#b` block user function                 |   N   |     N     |
+| Code `#l` takes user back to lobby            |   N   |     N     |
+| Code `#mc` to make chatroom                   |   N   |     N     |
+| Code `#dc` to delete chatroom                 |   N   |     N     |
+| Code `#lo` to log user out                    |   N   |     N     |
+| Code `#f` to transfer file                    |   N   |     N     |
+| Code `#sc` to show available chatrooms        |   N   |     N     |
+| Add date, time, and username to message       |   Y   |     Y     |
+| Code help-menu                                |   Y   |     Y     |
+| Learn GNU Readline Library                    |   N   |     N     |
+| Implement spell check                         |   N   |     N     |
+| Implement file transfer                       |   N   |     N     |
+| Create GUI                                    |   Y   |     N     |
+| Implement tab completion                      |   N   |     N     |
+| Implement nick hash function                  |   N   |     N     |
+| Determine how to store persistent information |   N   |     N     |
+| Determine how to store persistent information |   N   |     N     |
+| Integrate GUI and client application          |   Y   |     N     |
+| Send nicks to be verified by server           |   N   |     N     |
